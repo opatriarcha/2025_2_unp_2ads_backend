085d4ddca3d5dac11d816f5439f79d2f56e92efc
Orlando C. Patriarcha <orlando.patriarcha@gmail.com>
2025-09-24 22:00:21 -0300

    first integration test fopr post created

--- Blog/src/main/java/br/edu/unipaulistana/backend/Blog/controllers/UserController.java
+++ Blog/src/main/java/br/edu/unipaulistana/backend/Blog/controllers/UserController.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 //http://locallhost:8080/users
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
 
